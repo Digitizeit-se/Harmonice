a71fbd692d368339d00c969100073e2106725e54
John Larsen <john@pcis.se>
2023-10-09 00:37:28 +0200

    Moved File search to chained function classes

--- Harmonize/Loader.cs
+++ Harmonize/Loader.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Harmonize.IO;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using System.Reflection;
@@ -39,11 +40,11 @@ internal static class Loader
         {
             //Get logger factory from service provider
             var loggerFactory = services.GetService<ILoggerFactory>();
-            
+
             //Create logger , if not found create NullLogger
             _logger = loggerFactory?.CreateLogger("Loader")
-                         ?? new NullLoggerFactory().CreateLogger("Loader");
-            
+                      ?? new NullLoggerFactory().CreateLogger("Loader");
+
             //Get if is debug enabled
             _debug = _logger.IsEnabled(LogLevel.Debug); //or any other level
         }
@@ -51,19 +52,24 @@ internal static class Loader
         //Look if we already loaded the assembly earlier
         if (LoadedAssemblies.ContainsKey(assemblyName))
         {
-            if(_debug) _logger.LogDebug($"{assemblyName} loaded from cache.");
+            if (_debug) _logger.LogDebug($"{assemblyName} loaded from cache.");
 
             //Return the loaded assembly
             return LoadedAssemblies[assemblyName];
         }
 
-        //Get file location
-        var pluginLocation = FindFileLocation(rootFolder, assemblyName);
+
+        //Chain file finders to find file
+        var pathFinder = ChainFileFinders(_debug);
+
+        //Try to get file location
+        var pluginLocation = pathFinder.FindFileLocation(rootFolder, assemblyName);
 
         var loadContext = new LoaderContext(pluginLocation);
         if (_debug) _logger.LogDebug($"loading assembly: {assemblyName} from path: {pluginLocation}");
-        var assembly = loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
-        
+        var assembly =
+            loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+
         if (_debug) _logger.LogDebug($"Adding assembly {assembly.FullName} to cache.");
         LoadedAssemblies.Add(assemblyName, assembly);
 
@@ -71,118 +77,21 @@ internal static class Loader
     }
 
     /// <summary>
-    /// Find assembly location
+    /// Chain file finders
     /// </summary>
-    /// <param name="rootFolder">The plugin root folder </param>
-    /// <param name="assemblyName">The file assembly name</param>
-    /// <returns>file path if found</returns>
-    /// <exception cref="FileNotFoundException">when file can´t be found</exception>
-    private static string FindFileLocation(string rootFolder, string assemblyName)
+    /// <returns>FileLocationFinder with chained file finders</returns>
+    private static FileLocationFinder ChainFileFinders(bool isDebug)
     {
-        var pluginLocation = string.Empty;
-        var fileFound = false;
-
         //Get the execution / Entry application paths
         var entryRoot = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location);
         var executingRoot = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-
-        if (!fileFound)
-        {
-            if (entryRoot != null)
-                fileFound = FindFileInPluginNameFolder(entryRoot, rootFolder, assemblyName, out pluginLocation);
-        }
-
-        if (!fileFound)
-        {
-            if (executingRoot != null)
-                fileFound = FindFileInPluginNameFolder(executingRoot, rootFolder, assemblyName, out pluginLocation);
-        }
-
-        if (!fileFound)
-        {
-            if (entryRoot != null)
-                fileFound = FindFileInPluginRootFolder(entryRoot, rootFolder, assemblyName, out pluginLocation);
-        }
-
-        if (!fileFound)
-        {
-            if (executingRoot != null)
-                fileFound = FindFileInPluginRootFolder(executingRoot, rootFolder, assemblyName, out pluginLocation);
-        }
-
-        if (!fileFound)
-        {
-            if (entryRoot != null) fileFound = FindFileInApplicationFolder(entryRoot, assemblyName, out pluginLocation);
-        }
-
-        if (!fileFound)
-        {
-            if (executingRoot != null)
-                fileFound = FindFileInApplicationFolder(executingRoot, assemblyName, out pluginLocation);
-        }
-
-        if (fileFound) return pluginLocation;
-
-        //If not found at all then throw exception
-        _logger!.LogError($"Plug-in file: {assemblyName}.dll  not found in plug-in path. ");
-        throw new FileNotFoundException($"Plug-in file: {assemblyName}.dll  not found in any path. ");
+        return new FindFileInPluginNameFolder(
+            _logger!, entryRoot, new FindFileInPluginNameFolder(
+                _logger!, executingRoot, new FindFileInPluginRootFolder(
+                    _logger!, entryRoot, new FindFileInPluginRootFolder(
+                        _logger!, executingRoot, new FindFileInApplicationFolder(
+                            _logger!, entryRoot, new FindFileInApplicationFolder(
+                                _logger!, executingRoot,null,isDebug:isDebug),isDebug),isDebug),isDebug),isDebug),isDebug);
     }
-
-
-    /// <summary>
-    /// Validate if file exists in plugin name folder
-    /// </summary>
-    /// <param name="appRoot">Application root folder </param>
-    /// <param name="rootFolder">Plugins root folder </param>
-    /// <param name="assemblyName">The dll file </param>
-    /// <param name="pluginLocation">Out path if file was found</param>
-    /// <returns>true if found else false</returns>
-    private static bool FindFileInPluginNameFolder(string appRoot,string rootFolder, string assemblyName, out string pluginLocation)
-    {
-        //Try to find dll file in folder with same name as assembly
-        pluginLocation = Path.GetFullPath(Path.Combine(appRoot, Path.Combine(Path.Combine(rootFolder, assemblyName), $"{assemblyName}.dll"))).Replace('\\', Path.DirectorySeparatorChar);
-
-        if (_debug) _logger!.LogDebug($"look for Plug-in file: {assemblyName}.dll in path: {pluginLocation}");
-        
-        return File.Exists(pluginLocation);
-
-    }
-
-    /// <summary>
-    /// Validate if file exists in plugin root folder
-    /// </summary>
-    /// <param name="appRoot">Application root folder </param>
-    /// <param name="rootFolder">Plugins root folder </param>
-    /// <param name="assemblyName">The dll file </param>
-    /// <param name="pluginLocation">Out path if file was found</param>
-    /// <returns>true if found else false</returns>
-    private static bool FindFileInPluginRootFolder(string appRoot, string rootFolder, string assemblyName, out string pluginLocation)
-    {
-        //Try to find dll file in folder with same name as assembly
-        pluginLocation = Path.GetFullPath(Path.Combine(appRoot, Path.Combine(rootFolder, $"{assemblyName}.dll"))).Replace('\\', Path.DirectorySeparatorChar);
-
-        if (_debug) _logger!.LogDebug($"look for Plug-in file: {assemblyName}.dll in path: {pluginLocation}");
-
-        return File.Exists(pluginLocation);
-
-    }
-
-    /// <summary>
-    /// Validate if file exists in application folder
-    /// </summary>
-    /// <param name="appRoot">Application root folder </param>
-    /// <param name="assemblyName">The dll file </param>
-    /// <param name="pluginLocation">Out path if file was found</param>
-    /// <returns>true if found else false</returns>
-    private static bool FindFileInApplicationFolder(string appRoot, string assemblyName, out string pluginLocation)
-    {
-        //Try to find dll file in folder with same name as assembly
-        pluginLocation = Path.GetFullPath(Path.Combine(appRoot, $"{assemblyName}.dll")).Replace('\\', Path.DirectorySeparatorChar);
-
-        if (_debug) _logger!.LogDebug($"look for Plug-in file: {assemblyName}.dll in path: {pluginLocation}");
-
-        return File.Exists(pluginLocation);
-
-    }
-}
+}   
